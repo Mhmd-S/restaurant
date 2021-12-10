cae6079f29b01cbdbf78d53b28c6ad4148526264
Mhmd.S <mhmdslmn20@gmail.com>
2021-12-10 02:49:18 +0200

    type: added new features Added a style.css and add info inside wepab.config.js created the barebones for index.html

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,22 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    modules:{
+        rules: [
+            {
+                test:/\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test:/\.(png|svg|jpeg|jpg|gif)$/i,
+                type: "asset/resource"
+            },
+            {
+                test:/\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource"
+            }
+        ]
     }
 }
