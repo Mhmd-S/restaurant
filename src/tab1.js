1b3be8f0c8673fbe911752043b035b7e93bc6aaf
Mhmd.S <mhmdslmn20@gmail.com>
2021-12-14 19:07:14 +0200

    type: feature started with the menu section. added the basic format for the menu (divs and title)

--- src/tab1.js
+++ src/tab1.js
@@ -1,11 +1,32 @@
-import './style.css';
+// Menu Tab
+import './menu.css';
 
 function showTab1(){
+    // Assign an id for the main content section
     const divContent = document.getElementById('content');
 
+    // Left and right section of the menu
+    const div1 = document.createElement('div');
+    const div2 = document.createElement('div');
 
+    // Left section top and bottom
+    const div1Min1 = document.createElement('div');
+    const div1Min2 = document.createElement('div');
 
+    // Right section top and bottom
+    const div2Min1 = document.createElement('div');
+    const div2Min2 = document.createElement('div');
+    
+    // Appending two divs to the main section forming the left and right side.
+    divContent.appendChild(div1);
+    divContent.appendChild(div2);
 
+    // Appending two subsection to the right and two to the left.
+    // Right section
+    div1.appendChild(div1Min1);
+    div1.appendChild(div1Min2);
 
-
+    // Left section
+    div2.appendChild(div2Min1);
+    div2.appendChild(div2Min2);
 };
