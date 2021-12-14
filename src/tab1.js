30a794d6f9602a2ae88c0586c8a6e5dce2bea920
Mhmd.S <mhmdslmn20@gmail.com>
2021-12-14 19:42:07 +0200

    type: feature
    - Added comments in the menu javascript file for more clarification

--- src/tab1.js
+++ src/tab1.js
@@ -2,9 +2,14 @@
 import './menu.css';
 
 function showTab1(){
+    // ---------- ELEMENT ASSIGNMENT SECTION (START)-------------
     // Assign an id for the main content section
     const divContent = document.getElementById('content');
 
+    // ---------- ELEMENT ASSIGNMENT SECTION (END)-------------
+
+
+    // ---------- ELEMENT CREATION SECTION (START)------------------
     // Left and right section of the menu
     const div1 = document.createElement('div');
     const div2 = document.createElement('div');
@@ -17,6 +22,9 @@ function showTab1(){
     const div2Min1 = document.createElement('div');
     const div2Min2 = document.createElement('div');
     
+    // ---------- ELEMENT CREATION SECTION (END)------------------
+
+    // ---------- APPEND SECTION (START) ---------------
     // Appending two divs to the main section forming the left and right side.
     divContent.appendChild(div1);
     divContent.appendChild(div2);
@@ -29,4 +37,7 @@ function showTab1(){
     // Left section
     div2.appendChild(div2Min1);
     div2.appendChild(div2Min2);
+
+    // ---------- APPEND SECTION (END) ---------------
+
 };
